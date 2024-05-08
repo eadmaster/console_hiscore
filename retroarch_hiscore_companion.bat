fa0dc6eebca176af0853908c73a494c7fb6e8e8c
eadmaster <925171+eadmaster@users.noreply.github.com>
2024-05-08 16:26:28 +0200

    added retroarch_hiscore_companion launcher scripts

--- retroarch_hiscore_companion.bat
+++ retroarch_hiscore_companion.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set HISCORE_DAT_PATH=%~dp0plugins\hiscore\console_hiscore.dat
+set HISCORE_DAT_PATH=%~dp0console_hiscore.dat
 REM set HISCORE_PATH="R:\" 
 
 python "%~dp0tools\retroarch_hiscore_companion.py" %*
--- retroarch_hiscore_companion.sh
+++ retroarch_hiscore_companion.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-THIS_SCRIPT_DIR="$( cd $(dirname $0) ; pwd -P )"
+THIS_SCRIPT_DIR=$(dirname $(readlink -fn $0))
 
-export HISCORE_DAT_PATH="$THIS_SCRIPT_DIR/plugins/hiscore/console_hiscore.dat"
+export HISCORE_DAT_PATH="$THIS_SCRIPT_DIR/console_hiscore.dat"
 #export HISCORE_PATH="/r"
 
 cd "$THIS_SCRIPT_DIR/tools"
